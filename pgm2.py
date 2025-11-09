037ea1d4a8742391e791eef5228e455d0ea6d865
Mohammad Ansil :) <146548918+MasterOfBrokenLogic@users.noreply.github.com>
2025-11-09 19:22:44 +0530

    Create pgm2.py

---
+++ pgm2.py
@@ -0,0 +1,6 @@
+#Program to print m^n, where m and n are read from the user!
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = num1 ** num2
+print(f"m raised to n of {num1} & {num2} is: ", result)
